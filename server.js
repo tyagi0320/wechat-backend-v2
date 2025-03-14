d2f5252281b219af84d450917715f1e3786d4042
tyagi0320 <127464668+tyagi0320@users.noreply.github.com>
2025-03-14 20:03:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = socketIo(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "*",
         methods: ["GET", "POST"]
     }
 });
